8a50fee06016b5b7f6a1503ad61f91ec91131cfa
Oliver Pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2026-05-06 11:33:13 +0200

    Add Plop Audio

--- Assets/Scripts/Celebrator.cs
+++ Assets/Scripts/Celebrator.cs
@@ -5,14 +5,21 @@ public class Celebrator : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject Confetti;
+    private AudioSource _audioSource;
+    
     void Start()
     {
         SuikaBall.Merge.AddListener(OnMerge);
+        _audioSource = GetComponent<AudioSource>();
     }
 
     public void OnMerge(int Level, Vector3 Position){
-        Debug.Log("Der Celebrator hat ein Merge-Event wahrgenommen!");
         Position -= Vector3.forward;
+        const float minimumPitch = 2;
+        const float maximumPitch = 0.5f;
+        float t = Level / 10f;
+        _audioSource.pitch = Mathf.Lerp(minimumPitch, maximumPitch, t);
+        _audioSource.Play();
         for (int i = 0; i < Level * Level; i++){
             Instantiate(Confetti, Position, Quaternion.identity);
         }
